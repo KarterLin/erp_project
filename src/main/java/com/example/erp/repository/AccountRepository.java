b91717856ee18f2c96cd86b0834a36801466464c
willy96194 <willy9614@gmail.com>
2025-08-12 15:07:43 +0800

    prepaid expense account

--- src/main/java/com/example/erp/repository/AccountRepository.java
+++ src/main/java/com/example/erp/repository/AccountRepository.java
@@ -10,11 +10,19 @@ import org.springframework.stereotype.Repository;
 import com.example.erp.dto.AccountDTO;
 import com.example.erp.entity.Account;
 
+import io.lettuce.core.dynamic.annotation.Param;
+
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
 	Optional<Account> findByCode(String code);
+	@Query("SELECT a.code FROM Account a WHERE a.name = :name")
+	Optional<String> findCodeByName(@Param("name") String name);
+
 	@Query("SELECT new com.example.erp.dto.AccountDTO(a.code, a.name) FROM Account a WHERE a.id <> 65")
 	List<AccountDTO> findAllToDTO();
+
+	@Query("SELECT new com.example.erp.dto.AccountDTO(a.code, a.name) FROM Account a WHERE a.code IN :codes")
+	List<AccountDTO> findAccountsByCodes(@Param("codes") List<String> codes);
 
 
 }
