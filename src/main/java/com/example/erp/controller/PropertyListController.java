1ba93beb90260a4757af445e80765fb2220b7f03
willy96194 <willy9614@gmail.com>
2025-08-13 14:32:10 +0800

    PropertyList finished

--- src/main/java/com/example/erp/controller/PropertyListController.java
+++ src/main/java/com/example/erp/controller/PropertyListController.java
@@ -10,17 +10,31 @@ import com.example.erp.dto.PropertyListDTO;
 import com.example.erp.entity.Category;
 import com.example.erp.service.PropertyListService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
+@Tag(name = "Property List", description = "資產清單查詢 API")
 public class PropertyListController {
 	
 	@Autowired
 	private PropertyListService propertyListService;
 	
+	
+	 @Operation(
+		        summary = "取得固定資產清單",
+		        description = "回傳所有固定資產（Category = FIXED_ASSET）的清單資料"
+		    )
 	@GetMapping("api/fixed-asset")
 	public List<PropertyListDTO> getFAProperty(){
 		return propertyListService.getPropertyList(Category.FIXED_ASSET);
 	}
 
+	 
+	@Operation(
+		        summary = "取得無形資產清單",
+		        description = "回傳所有無形資產（Category = INTANGIBLE_ASSET）的清單資料"
+		    ) 
 	@GetMapping("api/intangible-asset")
 	public List<PropertyListDTO> getIAProperty(){
 		return propertyListService.getPropertyList(Category.INTANGIBLE_ASSET);
