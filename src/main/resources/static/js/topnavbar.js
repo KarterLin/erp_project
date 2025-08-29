0d2d54e46ebce8019d2a2dfb75b1d45e3befb1f6
Annie <annie91375@icloud.com>
2025-08-29 13:15:55 +0800

    AccountName顯示

--- src/main/resources/static/js/topnavbar.js
+++ src/main/resources/static/js/topnavbar.js
@@ -3,6 +3,38 @@ document.addEventListener("DOMContentLoaded", () => {
     .then(response => response.text())
     .then(data => {
       document.getElementById("topbar-container").innerHTML = data;
+
+      loadUser();
     })
     .catch(error => console.error("Topbar 載入失敗:", error));
 });
+
+
+// API endpoint
+const API_URL = "https://127.0.0.1:8443/api";
+
+async function loadUser() {
+  try {
+    const meRes = await fetch(`${API_URL}/me`, { credentials: "include" });
+    if (meRes.status === 401) {
+      // cookie 過期或未登入 → 直接跳轉
+      window.location.href = "login.html";
+      return;
+    }
+
+    if (!meRes.ok) throw new Error("取得登入者資訊失敗");
+
+    const meResult = await meRes.json();
+    console.log(meResult);
+    const account = meResult.data.account;
+    const userAccountEl = document.getElementById("userAccount");
+    if (userAccountEl) {
+      userAccountEl.textContent = account;
+    }
+
+  } catch (err) {
+    console.error("Error loading user detail:", err);
+    alert("載入使用者失敗");
+  }
+
+}
