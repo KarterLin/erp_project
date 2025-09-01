77912a10986e1038b6e776a8b44f55299e69ae25
willy96194 <willy9614@gmail.com>
2025-09-01 15:56:41 +0800

    09010356

--- src/main/resources/static/js/register.js
+++ src/main/resources/static/js/register.js
@@ -1,5 +1,5 @@
 // API endpoint
-const API_URL = "https://127.0.0.1:8443/api";
+const API_URL = "https://127.0.0.1:8443/api/register";
 
 // 啟用按鈕 
 const submitBtn = document.getElementById("submitBtn");
